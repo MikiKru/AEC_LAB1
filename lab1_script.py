d493ba6c0ab2bf4516901c779820cdecab0b10a4
MikiKru <michal_kruczkowski@o2.pl>
2019-05-06 11:57:02 +0200

    c3

--- lab1_script.py
+++ lab1_script.py
@@ -1,5 +1,12 @@
+from math import sin
+
+
 class SignalAnalysis:
     def signals_generator(self):
         self.sinus_signal = []
+        i = 0
+        while (len(self.sinus_signal) < 1000):
+            self.sinus_signal.append(sin(i/120))
+            i += 1
 
 
